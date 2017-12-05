6ce36f48c8600888147868dac7cb3e8cafe73245
jhoke97 <jhokanson1@ecc-jaguar.csuchico.edu>
2017-12-04 18:36:53 -0800

    skeleton

---
+++ account.cpp
@@ -0,0 +1,41 @@
+#include "student.h"
+#include <fstream>
+#include <string>
+account::account()
+{
+  accountName = "Unknown";
+  balance = 0.00;
+}
+void account::withdraw(double amount)
+{
+
+}
+void account::deposit(double amount)
+{
+
+}
+double account::viewBalance()
+{
+
+}
+void account::openAccount(std::string AccountName)
+{
+
+}
+void account::closeAccount()
+{
+
+}
+void account::displayAccounts()
+{
+
+}
+void createAccount()
+{
+  cout << "CREATE NEW ACCOUNT\n";
+  cout << "Enter new username: ";
+  cin >> username;
+  cout << endl << "Enter password: ";
+  cin >>password;
+  cout << endl << "Confirm password: "
+}
